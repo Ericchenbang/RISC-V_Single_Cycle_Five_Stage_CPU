ea4f4e782e88bb902e25ed977e081c3895d9a8ee
Ericchenbang <ericchenbsng@gmail.com>
2025-10-14 10:41:07 +0800

    Modify matching rule from funct3

--- ALU.v
+++ ALU.v
@@ -1,23 +1,23 @@
 module ALU(x, y, result, ALUControl, zero)
 input [31:0] x;
 input [31:0] y;
-input [4:0] ALUControl;
+input [3:0] ALUControl;
 output reg [31:0] result;
 output reg zero;
 
 
 always@(*) begin
     case(ALUControl)
-        5'd0: result = x & y;   // AND, ANDI
-        5'd1: result = x | y;   // OR, ORI
-        5'd2: result = x + y;   // ADD, ADDI
-        5'd3: result = x - y;   // SUB, BEQ, BNE
-        5'd4: result = x ^ y;   // XOR, XORI
-        5'd5: result = ($signed(x) < $signed(y)) ? 1 : 0;   // SLT, SLTI
-        5'd6: result = ($unsigned(x) < $unsigned(y)) ? 1:0; // SLTU, SLTIU
-        5'd7: result = $unsigned(x) << y[4:0];             // SLL, SLLI
-        5'd8: result = $unsigned(x) >> y[4:0];  // SRL, SRLI
-        5'd9: result = $signed(x) >> y[4:0];    // SRA, SRAI
+        4'd0: result = x + y;   // ADD, ADDI
+        4'd8: result = x - y;   // SUB, BEQ, BNE
+        4'd1: result = $unsigned(x) << y[4:0];             // SLL, SLLI
+        4'd2: result = ($signed(x) < $signed(y)) ? 1 : 0;   // SLT, SLTI
+        4'd3: result = ($unsigned(x) < $unsigned(y)) ? 1:0; // SLTU, SLTIU
+        4'd4: result = x ^ y;   // XOR, XORI
+        4'd5: result = $unsigned(x) >> y[4:0];  // SRL, SRLI
+        4'd9: result = $signed(x) >> y[4:0];    // SRA, SRAI
+        4'd6: result = x | y;   // OR, ORI
+        4'd7: result = x & y;   // AND, ANDI
         default: result = 0;
     endcase
 end
